4f299585b3c945e63689114b67c752f4a5592f39
Jessa Marie Barre <jmlbarre@gmail.com>
2017-08-31 12:39:06 -0400

    started shifting from jquery to angular

--- public/app.module.js
+++ public/app.module.js
@@ -1,9 +1,32 @@
-(function () {
-  angular.module('chattyApp', ['ui.router'])
-  .controller('ChattyController', [ChattyController])
+/* global angular $ io */
 
-  function ChattyController () {
+(function () {
+  var socket = io()
+
+  angular.module('chattyApp', [])
+  .controller('ChattyController', ['$scope', ChattyController])
+
+  function ChattyController ($scope) {
     // contains all incoming messages
+    let self = this
     this.messages = []
+
+    // append to page
+    socket.on('chat message', function (msg) {
+      self.messages.push(msg)
+      console.log(msg)
+      // Socket events will not trigger a state change in Angular.
+      // so we need to force a state change
+      $scope.$apply()
+    })
+
+    $(function () {
+      // load the socket.io-client
+      $('form').submit(function (e) {
+        socket.emit('chat message', $('#m').val())
+        $('#m').val('')
+        return false
+      })
+    })
   }
-})
+})()
